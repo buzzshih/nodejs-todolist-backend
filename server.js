4807d1127bc9492b64ac22a2b9f2bc166541d2c0
yuhsiang1996 <yuhsiang1996@gmail.com>
2024-07-10 13:45:07 +0800

    modify server port setting

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-const Joi = require("joi");
-const express = require("express");
+const Joi = require('joi');
+const express = require('express');
 const app = express();
-const path = require("path");
-const db = require("./src/db");
+const path = require('path');
+const db = require('./src/db');
 // const cors = require("cors");
-require("dotenv").config();
+require('dotenv').config();
 
 app.use(express.json());
 
 console.log(process.env.ENV);
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
-app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
-app.get("/api/todos", db.getTodos);
+app.get('/api/todos', db.getTodos);
 
-app.get("/api/todos/:id", db.getTodoById);
+app.get('/api/todos/:id', db.getTodoById);
 
-app.post("/api/todos", db.createTodo);
+app.post('/api/todos', db.createTodo);
 
-app.put("/api/todos/:id", db.updateTodo);
+app.put('/api/todos/:id', db.updateTodo);
 
-app.put("/api/todosDone/:id", db.updateTodoDone);
+app.put('/api/todosDone/:id', db.updateTodoDone);
 
-app.delete("/api/todos/:id", db.deleteTodo);
+app.delete('/api/todos/:id', db.deleteTodo);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listen to ${port} port`));
 
